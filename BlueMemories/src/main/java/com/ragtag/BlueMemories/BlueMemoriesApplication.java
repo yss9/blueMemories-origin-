39febb1119b385c7bc8070ce1f73a3b27d5d07c1
groovy-sky-jyun <because319@gmail.com>
2023-12-29 23:51:34 +0900

    first
    기본 세팅

---
+++ BlueMemories/src/main/java/com/ragtag/BlueMemories/BlueMemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.ragtag.BlueMemories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueMemoriesApplication.class, args);
+	}
+
+}
---
+++ BlueMemories/src/test/java/com/ragtag/BlueMemories/BlueMemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragtag.BlueMemories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueMemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
