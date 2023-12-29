dc2e7099a844d613f18d1b76edcacc9cda2ddffc
yss9 <yse2196@gmail.com>
2023-12-30 00:48:39 +0900

    연동추가
    받아랏!

---
+++ BlueMemories/src/main/java/com/ragtag/BlueMemories/domain/Member.java
@@ -0,0 +1,15 @@
+package com.ragtag.BlueMemories.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data            // Getter Setter
+@Builder        // DTO -> Entity화
+@AllArgsConstructor    // 모든 컬럼 생성자 생성
+
+
+public class Member {
+
+}
