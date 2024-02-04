13cc2cd6217c53c27ea37786205dd5c9af386234
jyun <because97@naver.com>
2024-02-04 18:17:00 +0900

    소설작성 UI 작업중

--- BlueMemories/src/main/frontend/src/pages/storageNovel/StorageNovelPage.js
+++ BlueMemories/src/main/frontend/src/pages/storageNovel/StorageNovelPage.js
@@ -16,7 +16,7 @@ const Wrapper = styled.div`
 const TextBtnContainer=styled.div`
     /*위치*/
     width:16%;
-    height: 89.3%;
+    height: 34vw;
     margin-right:0.5%;
     margin-top:0.25%;
     /*png*/
@@ -32,7 +32,8 @@ const TextBtnContainer=styled.div`
 `;
 const TextNickNameContainer=styled.div`
 
-`;
+`
+//----------------------------------------------------------------------;
 const MenuBtn=styled.button`
     /*스타일*/
     border: none;
@@ -47,13 +48,76 @@ const MenuBtn=styled.button`
     background-color:transparent;
 `;
 //---------------------------------------------------------------
-const ListContainer=styled.div`
+const ListBackgroundImg=styled.div`
     width:47%;
-    height: 90%;
+    height: 34vw;
     background-image: url("/resourcesPng/storagePage/storage_list_background.png");
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
+    align-items: center;
+`;
+//--------------------------------------------
+const ListContainer=styled.div`
+    width:90%;
+    height: 90%;
+    margin-left: 5%;
+    margin-top: 4%;
+   //background-color: blue;
+    /*item정렬*/
+    display: flex;
+    flex-direction: column;
+`;
+const ListStateText=styled.text`
+    font-size: 1.7vw;
+    color: #436052;
+    font-family: gangwonedusaeeum;
+    margin-left:2%;
+    margin-top: 1%;
+    /*밑줄 옵션*/
+    text-decoration: underline;
+    text-decoration-thickness: 4%;
+    text-underline-position:under;
+`;
+const BookListContainer=styled.div`
+    width: ${(props)=>props.width||'100%'};
+    height: ${(props)=>props.height||'30%'};
+    margin-top: ${(props)=>props.margin_top||'3%'};
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+    //background-color: yellow;
+`;
+const AddBookItem=styled.button`
+    width: 14%;
+    height: 100%;
+    background-image: url("/resourcesPng/storagePage/storageNovelPage/add_novel_btn.png");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-color:transparent;
+    //background-color: green;
+    border: none;
+`;
+
+const BookItemTitle=styled.text`
+    width: 13%;
+    height: 100%;
+    //background-color: red;
+    font-size: 1.1vw;
+    color: #436052;
+    font-family: gangwonedusaeeum;
+    text-align: center;
+`;
+const BookListNextBtn=styled.button`
+    width: 4%;
+    height: 35%;
+    background-image: url("/resourcesPng/storagePage/storageNovelPage/book_list_next_btn.png");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-color:transparent;
+    border: none;
 `;
 //------------------------------------------------------------------
 const ContentContainer=styled.div`
@@ -81,9 +145,43 @@ const StorageNovelForm = () => {
                         <MenuBtn>소설</MenuBtn>
                         <MenuBtn>그림책</MenuBtn>
                     </TextBtnContainer>
-                    <ListContainer></ListContainer>
+                    <ListBackgroundImg>
+                        <ListContainer>
+                            <ListStateText>작성 중</ListStateText>
+                            <BookListContainer>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <BookListNextBtn></BookListNextBtn>
+                            </BookListContainer>
+                            <BookListContainer width='92%' height='15%' margin_top='0%'>
+                                <BookItemTitle></BookItemTitle>
+                                <BookItemTitle>비밀의 정원</BookItemTitle>
+                                <BookItemTitle>노르웨이숲의 비밀 사건</BookItemTitle>
+                                <BookItemTitle></BookItemTitle>
+                                <BookItemTitle></BookItemTitle>
+                            </BookListContainer>
+                            <ListStateText>작성 완료</ListStateText>
+                            <BookListContainer>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <AddBookItem></AddBookItem>
+                                <BookListNextBtn></BookListNextBtn>
+                            </BookListContainer>
+                            <BookListContainer width='92%' height='15%' margin_top='0%'>
+                                <BookItemTitle></BookItemTitle>
+                                <BookItemTitle>비밀의 정원</BookItemTitle>
+                                <BookItemTitle>노르웨이숲의 비밀 사건</BookItemTitle>
+                                <BookItemTitle></BookItemTitle>
+                                <BookItemTitle></BookItemTitle>
+                            </BookListContainer>
+                        </ListContainer>
+                    </ListBackgroundImg>
                 </ContentContainer>
-                
             </Wrapper>
         </div>
     );
